3d84661eea5c7410f05322de1839aa4ac8f29479
Alcides Neto <=>
2021-09-09 10:29:46 -0300

    Faz requisição com query filter

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -8,7 +8,7 @@ export class OfertasService {
 
   public getOfertas(): Promise<Oferta[]> {
     return this.http
-      .get('http://localhost:3000/ofertas')
+      .get('http://localhost:3000/ofertas?destaque=true')
       .toPromise()
       .then((resposta: any) => resposta);
   }
