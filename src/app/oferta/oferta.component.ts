33d7f28bdee234128f89d6a3ddda29aabf9e0421
Alcides Neto <=>
2021-09-10 14:37:25 -0300

    Elemento observável emitindo evento a cada 2 segundos

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { interval } from 'rxjs';
+// import { Observable, interval } from 'rxjs';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 
@@ -34,5 +36,10 @@ export class OfertaComponent implements OnInit {
     //     console.log('processamento foi classificado como');
     //   }
     // );
+
+    let tempo = interval(2000)
+    tempo.subscribe((intervalo: number) => {
+      console.log(intervalo)
+    })
   }
 }
