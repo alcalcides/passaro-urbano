adc2992bd58f6806f251d008c6fe549c14f87705
Alcides Neto <=>
2021-09-10 17:06:43 -0300

    Faz a pesquisa inteligente

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [OfertasService],
 })
 export class TopoComponent implements OnInit {
+  public ofertas!: Observable<Oferta[]>
+  
+  constructor(private ofertasService: OfertasService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  // public pesquisa(termoDaBusca: string): void {
+  //   this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
+  //   this.ofertas.subscribe(
+  //     (data: Oferta[]) => console.log(data);
+  //   )
+  // }
 
   public pesquisa(termoDaBusca: string): void {
-    console.log(termoDaBusca);
+    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca)
+    this.ofertas.subscribe(
+      (ofertas: Oferta[]) => console.log(ofertas)
+      )
   }
-
 }
