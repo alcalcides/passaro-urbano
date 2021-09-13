f42a8785c936aff134a4acb441a60af0b0785639
Alcides Neto <=>
2021-09-13 15:20:28 -0300

    Conserta a rota filha da aba 'como usar'

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasService } from 'src/app/ofertas.service';
 
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
   styleUrls: ['./como-usar.component.css'],
-  providers: [OfertasService]
+  providers: [OfertasService],
 })
 export class ComoUsarComponent implements OnInit {
-
-  public comoUsar: string = ''
+  public comoUsar: string = '';
 
   constructor(
-        private route: ActivatedRoute, 
-        private ofertasService: OfertasService
-  ) { }
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) {}
 
-  ngOnInit(): void {  
-    this.ofertasService.getComoUsarOfertaPorId(this.route.parent?.snapshot.params['id']) 
-      .then((descricao: string) => {
-        this.comoUsar = descricao
-      })                   
+  ngOnInit(): void {
+    this.route.parent?.params.subscribe((parametros: Params) => {
+      this.ofertasService
+        .getComoUsarOfertaPorId(parametros.id)
+        .then((descricao: string) => {
+          this.comoUsar = descricao;
+        });
+    });
   }
-
 }
