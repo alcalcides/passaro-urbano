2f1cb86b0d447ed9d682cac032fa776edf500f9c
Alcides Neto <=>
2021-09-14 12:45:09 -0300

    Insere registros de pedido no banco de dados

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,8 +1,26 @@
-import { Pedido } from "./shared/pedido.model";
+import { Injectable } from '@angular/core';
+import { Pedido } from './shared/pedido.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { URL_API } from './app.api';
+import { map } from 'rxjs/operators';
 
+@Injectable()
 export class OrdemCompraService {
+  constructor(private http: HttpClient) {}
 
-  public efetivarCompra(pedido: Pedido): void {
+  public efetivarCompra(pedido: Pedido): Observable<any> {
     console.log(pedido);
+    let headers: HttpHeaders = new HttpHeaders();
+    headers.append('Content-type', 'application/json');
+
+    return this.http.post(
+      `${URL_API}/pedidos`,
+      pedido,
+      {headers: headers}
+    )
+    .pipe(map((resposta: any) => {
+      console.log(resposta);
+    }))
   }
 }
