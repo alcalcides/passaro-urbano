7e1acfc67111ac05a7cb9ab9b4bc0c5ada950277
Alcides Neto <=>
2021-09-14 17:36:22 -0300

    Adiciona material do professor

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,35 +1,21 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { OrdemCompraService } from '../ordem-compra.service';
-import { Pedido } from '../shared/pedido.model';
+import { Component, OnInit } from '@angular/core';
+import { OrdemCompraService } from '../ordem-compra.service'
+import { Pedido } from '../shared/pedido.model'
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [OrdemCompraService],
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
-  @ViewChild('formulario') public f!: NgForm;
 
-  public idPedidoCompra!: number;
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
-  constructor(private ordemCompraService: OrdemCompraService) {}
+  ngOnInit() {
+    
+  }
 
-  ngOnInit() {}
-
-  public confirmarCompra() {
-    let pedido: Pedido = new Pedido(
-      this.f.value.endereco,
-      this.f.value.numero,
-      this.f.value.complemento,
-      this.f.value.formaPagamento
-    );
-
-    this.ordemCompraService
-      .efetivarCompra(pedido)
-      .subscribe((idPedido: number) => {
-        this.idPedidoCompra = idPedido;
-      });
+  public confirmarCompra(): void {
   }
 }
