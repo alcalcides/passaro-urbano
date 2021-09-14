26438d09d5cbc331b3cc299549a8f6632ff867fa
Alcides Neto <=>
2021-09-14 10:02:27 -0300

    Configura validacao neutra do estado inicial do formulário

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -16,27 +16,39 @@ export class OrdemCompraComponent implements OnInit {
   public complementoValido!: boolean;
   public formaPagamentoValido!: boolean;
 
+  public enderecoEstadoPrimitivo: boolean = true;
+  public numeroEstadoPrimitivo: boolean = true;
+  public complementoEstadoPrimitivo: boolean = true;
+  public formaPagamentoEstadoPrimitivo: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
+    this.enderecoEstadoPrimitivo = false;
     this.enderecoValido = this.endereco.length > 3 ? true : false;
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero;
+    this.numeroEstadoPrimitivo = false;
     this.numeroValido = this.numero.length >= 1 ? true : false;
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento;
+    this.complementoEstadoPrimitivo = false;
     this.complementoValido = this.complemento.length >= 1 ? true : false;
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
-    this.formaPagamentoValido = this.formaPagamento === 'dinheiro' || this.formaPagamento === 'debito'  ? true : false;
+    this.formaPagamentoEstadoPrimitivo = false;
+    this.formaPagamentoValido =
+      formaPagamento === 'dinheiro' || this.formaPagamento === 'debito'
+        ? true
+        : false;
   }
 }
