8e3e23e7404a88c65b913af36f606865187b5ce8
Alcides Neto <=>
2021-09-15 16:49:29 -0300

    Solução do prof

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,65 +1,65 @@
-import { CarrinhoService } from './../carrinho.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { OrdemCompraService } from '../ordem-compra.service';
-import { Pedido } from '../shared/pedido.model';
+import { OrdemCompraService } from '../ordem-compra.service'
+import { Pedido } from '../shared/pedido.model'
+import  {CarrinhoService}  from '../carrinho.service'
 import { ItemCarrinho } from '../shared/item-carrinho.model';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [OrdemCompraService],
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
-  public idPedidoCompra!: number;
-  public itensCarrinho: ItemCarrinho[] = [];
+
+  public idPedidoCompra!: number
+  public itensCarrinho: ItemCarrinho[] = []
 
   public formulario: FormGroup = new FormGroup({
-    endereco: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(120),
-    ]),
-    numero: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(1),
-      Validators.maxLength(8),
-    ]),
-    complemento: new FormControl(null),
-    formaPagamento: new FormControl(null, [Validators.required]),
-  });
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+    'complemento': new FormControl(null),
+    'formaPagamento':new FormControl(null, [Validators.required])
+  })
 
   constructor(
-    private ordemCompraService: OrdemCompraService,
-    public carrinhoService: CarrinhoService
-  ) {}
+              private ordemCompraService: OrdemCompraService,
+              public carrinhoService: CarrinhoService
+    ) { }
 
   ngOnInit() {
-    this.itensCarrinho = this.carrinhoService.exibirItens();
-    console.log(this.itensCarrinho);
+      this.itensCarrinho = this.carrinhoService.exibirItens()
+      console.log(this.itensCarrinho)
   }
 
   public confirmarCompra(): void {
-    let pedido: Pedido = new Pedido(
-      this.formulario.value.endereco,
-      this.formulario.value.numero,
-      this.formulario.value.complemento,
-      this.formulario.value.formaPagamento
-    );
-    this.ordemCompraService
-      .efetivarCompra(pedido)
-      .subscribe((idPedido: number) => {
-        this.idPedidoCompra = idPedido;
-      });
+    if(this.formulario.status === 'INVALID'){
+      console.log('formulario esta invalido')
+      this.formulario.get('endereco')?.markAsTouched()
+      this.formulario.get('numero')?.markAsTouched()
+      this.formulario.get('complemento')?.markAsTouched()
+      this.formulario.get('formaPagamento')?.markAsTouched()
+    }else {
+      let pedido: Pedido = new Pedido(
+        this.formulario.value.endereco, 
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+        )
+      this.ordemCompraService.efetivarCompra(pedido)
+        .subscribe((idPedido: number) => {
+            this.idPedidoCompra = idPedido
+        })
+    }
   }
 
   public adicionar(item: ItemCarrinho): void {
-    this.carrinhoService.adicionarQuantidade(item);
+    this.carrinhoService.adicionarQuantidade(item)
   }
 
-  public remover(item: ItemCarrinho): void {
-    this.carrinhoService.diminuirQuantidade(item);
+  public diminuir(item: ItemCarrinho): void {
+    this.carrinhoService.diminuirQuantidade(item)
   }
 
 }
